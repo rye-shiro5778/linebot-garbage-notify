fe934f069deb11e9e68cf216465cfca54818808c
rye-shiro5778 <qd30755778@outlook.jp>
2022-06-11 12:14:47 +0900

    prettier設定

--- src/main.ts
+++ src/main.ts
@@ -6,68 +6,68 @@ import { getLineTargets } from "./utils/line/getLineTargets";
 import "dayjs/locale/ja";
 
 export const handler = async function (event: any) {
-  const targets = getLineTargets();
+    const targets = getLineTargets();
 
-  if (!targets) {
+    if (!targets) {
+        return;
+    }
+
+    // lambda上では9時間の時差
+    const now = dayjs().add(9, "h");
+    const garbage = getGarbage(now);
+
+    const messages = garbage
+        ? [
+              {
+                  type: "text",
+                  text: `今日は「${garbage}」のゴミ捨てにいくんだ！！`,
+                  quickReply: {
+                      items: [
+                          {
+                              type: "action",
+                              action: {
+                                  type: "message",
+                                  label: "捨てたよ！！",
+                                  text: "捨てたよ！！",
+                              },
+                          },
+                          {
+                              type: "action",
+                              action: {
+                                  type: "message",
+                                  label: "今日はゴミなし、、",
+                                  text: "今日はゴミなし、、",
+                              },
+                          },
+                          {
+                              type: "action",
+                              action: {
+                                  type: "message",
+                                  label: "諦めたよ、、",
+                                  text: "諦めたよ、、",
+                              },
+                          },
+                      ],
+                  },
+              },
+          ]
+        : [
+              {
+                  type: "text",
+                  text: `今日はゴミ捨てはありません！`,
+              },
+          ];
+
+    const lineData: LineData = {
+        type: "multicast",
+        to: targets,
+        messages,
+    };
+
+    await sendLineMsg(lineData);
     return;
-  }
-
-  // lambda上では9時間の時差
-  const now = dayjs().add(9, "h");
-  const garbage = getGarbage(now);
-
-  const messages = garbage
-    ? [
-        {
-          type: "text",
-          text: `今日は「${garbage}」のゴミ捨てにいくんだ！！`,
-          quickReply: {
-            items: [
-              {
-                type: "action",
-                action: {
-                  type: "message",
-                  label: "捨てたよ！！",
-                  text: "捨てたよ！！",
-                },
-              },
-              {
-                type: "action",
-                action: {
-                  type: "message",
-                  label: "今日はゴミなし、、",
-                  text: "今日はゴミなし、、",
-                },
-              },
-              {
-                type: "action",
-                action: {
-                  type: "message",
-                  label: "諦めたよ、、",
-                  text: "諦めたよ、、",
-                },
-              },
-            ],
-          },
-        },
-      ]
-    : [
-        {
-          type: "text",
-          text: `今日はゴミ捨てはありません！`,
-        },
-      ];
-
-  const lineData: LineData = {
-    type: "multicast",
-    to: targets,
-    messages,
-  };
-
-  await sendLineMsg(lineData);
-  return;
 };
 
 if (process.env.NODE_ENV === "test") {
-  handler({});
+    handler({});
 }
--- src/utils/line/isSignatured.ts
+++ src/utils/line/isSignatured.ts
@@ -1,15 +1,15 @@
 import crypto from "crypto";
 export const isSignatured = (body: any, headers: any) => {
-  const signature = crypto
-    .createHmac("sha256", process.env["LINE_TOKEN"] || "")
-    .update(body)
-    .digest("base64");
+    const signature = crypto
+        .createHmac("sha256", process.env["LINE_TOKEN"] || "")
+        .update(body)
+        .digest("base64");
 
-  const checkHeader = (headers || {})["X-Line-Signature"];
+    const checkHeader = (headers || {})["X-Line-Signature"];
 
-  if (signature === checkHeader) {
-    return true;
-  } else {
-    throw new Error("署名エラー");
-  }
+    if (signature === checkHeader) {
+        return true;
+    } else {
+        throw new Error("署名エラー");
+    }
 };
